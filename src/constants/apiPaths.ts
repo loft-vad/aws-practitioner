5995333c9dcb370de27b44aaaa9bc6513c503459
loft-vad <57989395+loft-vad@users.noreply.github.com>
2024-04-08 23:47:21 +0200

    Homework 5

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://t9a1hmgj10.execute-api.eu-central-1.amazonaws.com/dev/products",
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  import: "https://ct9nvxp93g.execute-api.eu-central-1.amazonaws.com/dev/import",
   bff: "https://t9a1hmgj10.execute-api.eu-central-1.amazonaws.com/dev",
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
